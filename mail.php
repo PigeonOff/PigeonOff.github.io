04e2be7020d96121024e572fb9c5027487e0c1b8
Kirill Goluboff <pgnoff@gmail.com>
2017-10-19 22:59:32 +0300

    mm

--- mail.php
+++ mail.php
@@ -11,7 +11,7 @@ $text = $_POST['user_text'];
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->Host = 'smtp.yandex.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'kirigoll@yandex.ru'; // Ваш логин от почты с которой будут отправляться письма
 $mail->Password = 'porshe'; // Ваш пароль от почты с которой будут отправляться письма

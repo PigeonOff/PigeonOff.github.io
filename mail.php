144f6dd0d4fb0523c725f9ae7041f20f38ae3685
Kirill Goluboff <pgnoff@gmail.com>
2017-10-20 22:04:10 +0300

    d

--- mail.php
+++ mail.php
@@ -1,123 +1,19 @@
+<meta http-equiv='refresh' content='1; url=http://pigeonoff.github.io'>
+<meta charset="UTF-8" />
 <?php
-class mail {
-	/**
-	* 
-	* @var string $smtp_username - Логин
-	* @var string $smtp_password - Пароль
-	* @var string $smtp_host - Хост
-	* @var string $smtp_from - От кого
-	* @var integer $smtp_port - Порт
-	* @var string $smtp_charset - Кодировка
-	*
-	*/   
-	public $smtp_username;
-	public $smtp_password;
-	public $smtp_host;
-	public $smtp_from;
-	public $smtp_port;
-	public $smtp_charset;
-	
-	public function __construct($smtp_username, $smtp_password, $smtp_host, $smtp_from, $smtp_port = 25, $smtp_charset = "utf-8") {
-		$this->smtp_username = $smtp_username;
-		$this->smtp_password = $smtp_password;
-		$this->smtp_host = $smtp_host;
-		$this->smtp_from = $smtp_from;
-		$this->smtp_port = $smtp_port;
-		$this->smtp_charset = $smtp_charset;
-	}
-	
-	/**
-	* Отправка письма
-	* 
-	* @param string $mailTo - Получатель письма
-	* @param string $subject - Тема письма
-	* @param string $message - Тело письма
-	* @param string $headers - Заголовки письма
-	*
-	* @return bool|string В случаи отправки вернет true, иначе текст ошибки    *
-	*/
-	function send($mailTo, $subject, $message, $headers) {
-		$contentMail = "Date: " . date("D, d M Y H:i:s") . " UT\r\n";
-		$contentMail .= 'Subject: =?' . $this->smtp_charset . '?B?'  . base64_encode($subject) . "=?=\r\n";
-		$contentMail .= $headers . "\r\n";
-		$contentMail .= $message . "\r\n";
-		
-		try {
-			if(!$socket = @fsockopen($this->smtp_host, $this->smtp_port, $errorNumber, $errorDescription, 30)){
-				throw new Exception($errorNumber.".".$errorDescription);
-			}
-			if (!$this->_parseServer($socket, "220")){
-				throw new Exception('Connection error');
-			}
-			
-			$server_name = $_SERVER["SERVER_NAME"];
-			fputs($socket, "HELO $server_name\r\n");
-			if (!$this->_parseServer($socket, "250")) {
-				fclose($socket);
-				throw new Exception('Error of command sending: HELO');
-			}
-			
-			fputs($socket, "AUTH LOGIN\r\n");
-			if (!$this->_parseServer($socket, "334")) {
-				fclose($socket);
-				throw new Exception('Autorization error');
-			}
-			
-			fputs($socket, base64_encode($this->smtp_username) . "\r\n");
-			if (!$this->_parseServer($socket, "334")) {
-				fclose($socket);
-				throw new Exception('Autorization error');
-			}
-			
-			fputs($socket, base64_encode($this->smtp_password) . "\r\n");
-			if (!$this->_parseServer($socket, "235")) {
-				fclose($socket);
-				throw new Exception('Autorization error');
-			}
-			
-			fputs($socket, "MAIL FROM: <".$this->smtp_username.">\r\n");
-			if (!$this->_parseServer($socket, "250")) {
-				fclose($socket);
-				throw new Exception('Error of command sending: MAIL FROM');
-			}
-			
-			$mailTo = ltrim($mailTo, '<');
-			$mailTo = rtrim($mailTo, '>');
-			fputs($socket, "RCPT TO: <" . $mailTo . ">\r\n");     
-			if (!$this->_parseServer($socket, "250")) {
-				fclose($socket);
-				throw new Exception('Error of command sending: RCPT TO');
-			}
-			
-			fputs($socket, "DATA\r\n");     
-			if (!$this->_parseServer($socket, "354")) {
-				fclose($socket);
-				throw new Exception('Error of command sending: DATA');
-			}
-			
-			fputs($socket, $contentMail."\r\n.\r\n");
-			if (!$this->_parseServer($socket, "250")) {
-				fclose($socket);
-				throw new Exception("E-mail didn't sent");
-			}
-			
-			fputs($socket, "QUIT\r\n");
-			fclose($socket);
-		} catch (Exception $e) {
-			return  $e->getMessage();
-		}
-		return true;
-	}
-	
-	private function _parseServer($socket, $response) {
-		while (@substr($responseServer, 3, 1) != ' ') {
-			if (!($responseServer = fgets($socket, 256))) {
-				return false;
-			}
-		}
-		if (!(substr($responseServer, 0, 3) == $response)) {
-			return false;
-		}
-		return true;
-	}
+if (isset($_POST['name'])) {$name = $_POST['name']; if ($name == '') {unset($name);}}
+if (isset($_POST['phone'])) {$phone = $_POST['phone']; if ($phone == '') {unset($phone);}}
+if (isset($_POST['message'])) {$message = $_POST['message']; if ($message == '') {unset($message);}}
+if (isset($name) && isset($phone) && isset($message)){
+$address = "pgnoff@gmail.com";
+$mes = "Имя: $name \nE-mail: $phone \nТекст: $message";
+$send = mail ($address,$phone,$message,"Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
+if ($send == 'true')
+{echo "Сообщение отправлено";}
+else {echo "Ой, что-то пошло не так";}
 }
+else
+{
+echo "Заполните все поля";
+}
+?>
